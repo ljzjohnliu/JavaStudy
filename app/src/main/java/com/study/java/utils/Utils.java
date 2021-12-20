b7bdfe65e34bc4e6833fb3a852f7333932442519
Liu <>
2021-12-20 14:18:58 +0800

    继承ViewGroup实现自定义，代码存在问题！

---
+++ app/src/main/java/com/study/java/utils/Utils.java
@@ -0,0 +1,12 @@
+package com.study.java.utils;
+
+public class Utils {
+
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int)((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)((maxValue + 1) * Math.random()) - (int)(maxValue * Math.random());
+        }
+        return arr;
+    }
+}
