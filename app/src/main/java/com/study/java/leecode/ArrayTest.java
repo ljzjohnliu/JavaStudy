c4fd1ce285c04c8d87669c7dde6687089be51a43
Liu <>
2022-01-14 18:56:31 +0800

    最长递增子序列

---
+++ app/src/main/java/com/study/java/leecode/ArrayTest.java
@@ -0,0 +1,100 @@
+package com.study.java.leecode;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class ArrayTest {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1, 0, 3, 5, 9, 12};
+        int result = search(nums, 9, 0, nums.length - 1);
+        System.out.println(result);
+    }
+
+    /**
+     * 题目1、给定一个n个元素有序的（升序）整型数组nums 和一个目标值target ，写一个函数搜索nums中的 target，如果目标值存在返回下标，否则返回 -1。
+     * 示例 1:
+     * 输入: nums = [-1,0,3,5,9,12], target = 9
+     * 输出: 4
+     * 解释: 9 出现在 nums 中并且下标为 4
+     * 示例2:
+     * 输入: nums = [-1,0,3,5,9,12], target = 2
+     * 输出: -1
+     * 解释: 2 不存在 nums 中因此返回 -1
+     * 提示：
+     * 你可以假设 nums中的所有元素是不重复的。
+     * n将在[1, 10000]之间。
+     * nums的每个元素都将在[-9999, 9999]之间。
+     */
+    public static int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return -1;
+    }
+
+    /**
+     * 递归实现
+     */
+    public static int search(int[] nums, int target, int start, int end) {
+        if (start == end) {
+            if (nums[start] == target) {
+                return start;
+            } else {
+                return -1;
+            }
+        }
+        int mid = start + (end - start) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            return search(nums, target, mid + 1, end);
+        } else {
+            return search(nums, target, start, mid);
+        }
+    }
+
+    /**
+     * 题目2、最长递增子序列
+     * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+     * 子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+     * 示例 1：
+     * 输入：nums = [10,9,2,5,3,7,101,18]
+     * 输出：4
+     * 解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
+     * 示例 2：
+     * 输入：nums = [0,1,0,3,2,3]
+     * 输出：4
+     * 示例 3：
+     * 输入：nums = [7,7,7,7,7,7,7]
+     * 输出：1
+     * 提示：
+     * 1 <= nums.length <= 2500
+     * -104 <= nums[i] <= 104
+     */
+//    [10,9,2,5,3,7,101,18]
+    public static int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) return 0;
+        int[] dp = new int[nums.length];
+        int res = 0;
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
